f4c119b56fa73332682567757f2d996c4a7057df
Prescott <carlsonprescott@gmail.com>
2023-06-01 18:48:09 -0700

    finishing phase 2

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 // import { AuthRoute, ProtectedRoute } from "../util/route_util";
+
+import GreetingContainer from './greeting/greeting_container';
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
 
 const App = () => (
   <div>
     <header>
-      {/* <Link to="/" className="header-link"> */}
-        <h1>YourBnB</h1>
-      {/* </Link> */}
+      <h1>Your BnB</h1>
+      <GreetingContainer />
     </header>
+
+    <Route path="/login" component={LogInFormContainer} />
+    <Route path="/signup" component={SignUpFormContainer} />
   </div>
 );
 
