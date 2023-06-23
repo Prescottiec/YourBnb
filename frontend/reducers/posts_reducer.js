e45fff554e44da2ae9796b67a59e18edf082d81d
Prescott <carlsonprescott@gmail.com>
2023-06-22 19:29:36 -0700

    setting up posts

--- frontend/reducers/posts_reducer.js
+++ frontend/reducers/posts_reducer.js
@@ -1,23 +1,10 @@
-import {
-  RECEIVE_REVIEW,
-  RECEIVE_POSTS,
-  RECEIVE_POST,
-} from "../actions/post_actions";
+import { RECEIVE_POSTS } from "../actions/post_actions";
 
 const postsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_POSTS:
       return action.posts;
-    case RECEIVE_POST:
-      const newPost = { [action.post.id]: action.post };
-      return Object.assign({}, state, newPost);
-    case RECEIVE_REVIEW:
-      const { review, average_rating } = action;
-      const newState = Object.assign({}, state);
-      newState[review.post_id].reviewIds.push(review.id);
-      newState[review.post_id].average_rating = average_rating;
-      return newState;
     default:
       return state;
   }
