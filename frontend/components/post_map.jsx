04830e5cfeb7d0ae6750926ed007767078a82fb0
Prescott <carlsonprescott@gmail.com>
2023-06-28 17:50:54 -0700

    API added and displayed

--- frontend/components/post_map.jsx
+++ frontend/components/post_map.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { withRouter } from "react-router-dom";
 
+const mapOptions = {
+  center: { lat: 37.7758, lng: -122.435 }, // this is SF
+  zoom: 13,
+};
 
 class PostMap extends React.Component {
   componentDidMount() {
-    
+    // set the map to show SF
+    // const mapOptions = {
+    //   center: { lat: 37.7758, lng: -122.435 }, // this is SF
+    //   zoom: 13,
+    // };
+
+    // wrap this.mapNode in a Google Map
+    // this.map = new google.maps.Map(this.mapNode, mapOptions);
+    const map = this.refs.map;
+    this.map = new google.maps.Map(map, mapOptions);
   }
 
   render() {
