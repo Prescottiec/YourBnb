6ada74726e9d70c6971745f235ddaba06cfbbde3
Prescott <carlsonprescott@gmail.com>
2023-06-30 14:37:41 -0700

    changing map marker setting to be able to drag

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -7,7 +7,18 @@ class MarkerManager {
   updateMarkers(posts) {
     console.log("time to update");
   }
-  
+
+  createMarkerFromPost(post) {
+    const position = new google.maps.LatLng(post.lat, post.lng);
+    const marker = new google.maps.Marker({
+      position,
+      map: this.map,
+      postId: post.id,
+    });
+
+    marker.addListener("click", () => this.handleClick(post));
+    this.markers[marker.postId] = marker;
+  }
 }
 
 export default MarkerManager;
