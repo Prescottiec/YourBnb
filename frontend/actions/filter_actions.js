9e2f1e7a51c275283210496b65a3bffae7364a1e
Prescott <carlsonprescott@gmail.com>
2023-07-07 17:59:59 -0700

    adding filters

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,12 +1,18 @@
 import { fetchPosts } from "./post_actions";
 
 export const UPDATE_FILTER = "UPDATE_FILTER";
+export const UPDATE_BOUNDS = "UPDATE_BOUNDS";
 
 export const changeFilter = (filter, value) => ({
   type: UPDATE_FILTER,
   filter,
   value,
 });
+
+export const updateBounds = (bounds) => ({
+  type: UPDATE_BOUNDS,
+  bounds
+})
 
 export const updateFilter = (filter, value) => (dispatch, getState) => {
   dispatch(changeFilter(filter, value));
