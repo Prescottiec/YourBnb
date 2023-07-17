79ab5eddf0d8d7c306227330ce95c86a4b340f78
Prescott <carlsonprescott@gmail.com>
2023-07-17 12:09:54 -0700

    adding selectPost to selectors

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,3 +1,10 @@
-export const asArray = ({ posts }) => (
-  Object.keys(posts).map((key) => posts[key])
-);
+export const selectPost = ({ posts }, postId) => {
+  return posts[postId] || { reviewIds: [] };
+};
+
+export const selectReviewsForPost = ({ posts, reviews }, post) => {
+  return post.reviewIds.map((reviewId) => reviews[reviewId]);
+};
+
+export const asArray = ({ posts }) =>
+  Object.keys(posts).map((key) => posts[key]);
